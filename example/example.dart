5f78463304a5cff1e46b34cb6c08fef5f79d27b4
Nightfeather <ss900328@gmail.com>
2020-05-24 00:02:21 +0800

    Fix syntax errors in example
    Forgot dart needs a main function

--- example/example.dart
+++ example/example.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 import 'package:hidapi_dart/hidapi_dart.dart';
 
-var hid = HID(idVendor: 0x0001, idProduct: 0x0001);
-hid.open();
-hid.write("\x01\x01a")
-String str = hid.read();
-stdout.writeln(str);
-hid.close();
+void main() async {
+  var hid = HID(idVendor: 0x0001, idProduct: 0x0001);
+  hid.open();
+  await hid.write("\x01\x01a");
+  String str = await hid.read();
+  stdout.writeln(str);
+  hid.close();
+}
