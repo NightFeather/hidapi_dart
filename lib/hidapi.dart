3ed8838c1a54445db1301808be7c0157d864b2f2
Nightfeather <ss900328@gmail.com>
2020-05-23 23:55:43 +0800

    dartfmt

--- lib/hidapi.dart
+++ lib/hidapi.dart
@@ -6,35 +6,46 @@ import './allocation.dart';
 // void pointer
 final Pointer<Void> nullptr = Pointer<Void>.fromAddress(0);
 
-final _hidapi = Platform.isLinux ? DynamicLibrary.open('libhidapi-libusb.so') : DynamicLibrary.open('hidapi.dll');
+final _hidapi = Platform.isLinux
+    ? DynamicLibrary.open('libhidapi-libusb.so')
+    : DynamicLibrary.open('hidapi.dll');
 
 typedef _HIDInitFnNative = Int32 Function();
 typedef _HIDInitFnDart = int Function();
-final _HIDInitFnDart _hidInit = _hidapi.lookupFunction<_HIDInitFnNative, _HIDInitFnDart>('hid_init');
+final _HIDInitFnDart _hidInit =
+    _hidapi.lookupFunction<_HIDInitFnNative, _HIDInitFnDart>('hid_init');
 
 typedef _HIDExitFnNative = Int32 Function();
 typedef _HIDExitFnDart = int Function();
-final _HIDExitFnDart _hidExit = _hidapi.lookupFunction<_HIDExitFnNative, _HIDExitFnDart>('hid_exit');
+final _HIDExitFnDart _hidExit =
+    _hidapi.lookupFunction<_HIDExitFnNative, _HIDExitFnDart>('hid_exit');
 
 typedef _OpenDeviceFnNative = Pointer Function(Uint16, Uint16, Pointer);
 typedef _OpenDeviceFnDart = Pointer Function(int, int, Pointer);
-final _OpenDeviceFnDart _openDevice = _hidapi.lookupFunction<_OpenDeviceFnNative, _OpenDeviceFnDart>('hid_open');
+final _OpenDeviceFnDart _openDevice =
+    _hidapi.lookupFunction<_OpenDeviceFnNative, _OpenDeviceFnDart>('hid_open');
 
 typedef _CloseDeviceFnNative = Void Function(Pointer);
 typedef _CloseDeviceFnDart = void Function(Pointer);
-final _CloseDeviceFnDart _closeDevice = _hidapi.lookupFunction<_CloseDeviceFnNative, _CloseDeviceFnDart>('hid_close');
+final _CloseDeviceFnDart _closeDevice = _hidapi
+    .lookupFunction<_CloseDeviceFnNative, _CloseDeviceFnDart>('hid_close');
 
 typedef _ReadDeviceFnNative = Int32 Function(Pointer, Pointer<Uint8>, Uint64);
 typedef _ReadDeviceFnDart = int Function(Pointer, Pointer<Uint8>, int);
-final _ReadDeviceFnDart _readDevice = _hidapi.lookupFunction<_ReadDeviceFnNative, _ReadDeviceFnDart>('hid_read');
+final _ReadDeviceFnDart _readDevice =
+    _hidapi.lookupFunction<_ReadDeviceFnNative, _ReadDeviceFnDart>('hid_read');
 
-typedef _ReadDeviceTimeoutFnNative = Int32 Function(Pointer, Pointer<Uint8>, Uint64, Int32);
-typedef _ReadDeviceTimeoutFnDart = int Function(Pointer, Pointer<Uint8>, int, int);
-final _ReadDeviceTimeoutFnDart _readDeviceTimeout = _hidapi.lookupFunction<_ReadDeviceTimeoutFnNative, _ReadDeviceTimeoutFnDart>('hid_read_timeout');
+typedef _ReadDeviceTimeoutFnNative = Int32 Function(
+    Pointer, Pointer<Uint8>, Uint64, Int32);
+typedef _ReadDeviceTimeoutFnDart = int Function(
+    Pointer, Pointer<Uint8>, int, int);
+final _ReadDeviceTimeoutFnDart _readDeviceTimeout = _hidapi.lookupFunction<
+    _ReadDeviceTimeoutFnNative, _ReadDeviceTimeoutFnDart>('hid_read_timeout');
 
 typedef _WriteDeviceFnNative = Int32 Function(Pointer, Pointer<Uint8>, Int32);
 typedef _WriteDeviceFnDart = int Function(Pointer, Pointer<Uint8>, int);
-final _WriteDeviceFnDart _writeDevice = _hidapi.lookupFunction<_WriteDeviceFnNative, _WriteDeviceFnDart>('hid_write');
+final _WriteDeviceFnDart _writeDevice = _hidapi
+    .lookupFunction<_WriteDeviceFnNative, _WriteDeviceFnDart>('hid_write');
 
 final hidInit = _hidInit;
 final hidExit = _hidExit;
@@ -52,41 +63,41 @@ class HID {
   int open() {
     Pointer<Uint8> buffer = nullptr.cast();
 
-    if(this.serial != null) {
+    if (this.serial != null) {
       allocate<Uint8>(count: this.serial.length);
       buffer.asTypedList(1024).setAll(0, this.serial.runes);
     }
 
     var ptr = openDevice(this.idVendor, this.idProduct, buffer);
-    if(ptr == nullptr) {
+    if (ptr == nullptr) {
       return -1;
     }
     this.device = ptr;
-    if(buffer.address != nullptr.address) {
+    if (buffer.address != nullptr.address) {
       free(buffer);
     }
     return 0;
   }
 
   void close() {
-    if(this.device != nullptr) {
+    if (this.device != nullptr) {
       closeDevice(this.device);
     }
   }
 
-  Future<String> read({ len: 1024, timeout: 0 }) async {
+  Future<String> read({len: 1024, timeout: 0}) async {
     Pointer<Uint8> buffer = allocate<Uint8>(count: len);
-    buffer.asTypedList(1024).fillRange(0,len-1,0);
+    buffer.asTypedList(1024).fillRange(0, len - 1, 0);
 
     String str = '';
     int ret = 0;
-    if(timeout > 0) {
+    if (timeout > 0) {
       ret = _readDeviceTimeout(this.device, buffer, len, timeout);
     } else {
       ret = _readDevice(this.device, buffer, len);
     }
 
-    if(ret <= 0) {
+    if (ret <= 0) {
       free(buffer);
       return str;
     }
@@ -97,15 +108,14 @@ class HID {
 
   void flush() {
     Pointer<Uint8> buffer = allocate<Uint8>(count: 1024);
-    buffer.asTypedList(1024).fillRange(0,1023,0);
-    while(_readDeviceTimeout(this.device, buffer, 1024, 100) <= 0)
-
-    free(buffer);
+    buffer.asTypedList(1024).fillRange(0, 1023, 0);
+    while (_readDeviceTimeout(this.device, buffer, 1024, 100) <= 0)
+      free(buffer);
   }
 
   Future<int> write(String data) async {
     assert(data.length < 256);
-    int bufferSize = data.length+2;
+    int bufferSize = data.length + 2;
     Pointer<Uint8> buffer = allocate<Uint8>(count: bufferSize);
     var array = buffer.asTypedList(bufferSize);
     array.setAll(0, data.runes);
