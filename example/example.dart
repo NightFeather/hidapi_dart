02c942acc7fea47cb50cab2b8b5928296622d923
Nightfeather <ss900328@gmail.com>
2020-05-25 12:32:36 +0800

    Complete the example
    add the open check
    add the pubspec.yaml for the example

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,10 @@ import 'package:hidapi_dart/hidapi_dart.dart';
 
 void main() async {
   var hid = HID(idVendor: 0x0001, idProduct: 0x0001);
-  hid.open();
+  if(hid.open() < 0) {
+    stdout.writeln('HID Device open failed!');
+    return;
+  }
   await hid.write("\x01\x01a");
   String str = await hid.read();
   stdout.writeln(str);
